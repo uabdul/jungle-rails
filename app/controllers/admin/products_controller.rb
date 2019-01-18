3f17fed9a5e57b1c1c3e694d231ed44bd4e4f142
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-18 11:15:19 +0000

    Adding index, new and create actions for admin/categories.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -27,7 +27,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(
+    params.require(:category).permit(
       :name,
       :description,
       :category_id,
